b4340c3a3ba6d3e5d9d2ea02a676cc90b2901168
unknown <doubleugeun@gmail.com>
2021-01-26 02:12:09 -0800

    initial commit

---
+++ Side/Assets/Scripts/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Controller Start!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
